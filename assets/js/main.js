7b373c6ff0ea949372d7ca6f4e141471ed2c4337
Ella Choy <egal21de@gmail.com>
2021-07-21 02:19:03 +0200

    onclick

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+function changeColors1(){
+    document.body.style.backgroundColor = "rgb(84, 142, 87)"
+    document.body.style.color = "rgb(91, 41, 98)"
+}
+function changeColors2() {
+    document.body.style.backgroundColor = "rgb(70, 68, 111)"
+    document.body.style.color = "rgb(126, 110, 94)"
+}
+function changeColors3(){
+    document.body.style.backgroundColor = "rgb(0, 0, 36)"
+    document.body.style.color = "rgb(144, 74, 49)"
+}
+function changeColors4() {
+    document.body.style.backgroundColor = "rgb(12, 11, 107)"
+    document.body.style.color = "rgb(122, 206, 252)"
+}
